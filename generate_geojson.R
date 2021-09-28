5657fc0202487e2ca31a345faf33aab778bbf07c
Tyler Neumann <neumanntylers@gmail.com>
2021-09-28 19:16:39 -0400

    Writes as TopoJSON instead of geojson

--- generate_geojson.R
+++ generate_geojson.R
@@ -13,7 +13,12 @@ all_states <- states(cb = T, resolution = "20m") |>
 all_states_df <- all_states[, .(Name = NAME)] |> as.data.frame()
 row.names(all_states_df) <- gsub("^", "ID", row.names(all_states_df))
 
+
+file_conn <- file("geojson/states.json")
 all_states[, geometry] |> 
   as_Spatial() |> 
   SpatialPolygonsDataFrame(data = all_states_df) |>
-  geojson_write(file = "geojson/states")
+  geojson_json() |>
+  geo2topo(object_name = "State") |>
+  writeLines(file_conn)
+close(file_conn)
