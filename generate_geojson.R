372fbf12cfdf8b8691ead5290620402e252e6539
Tyler Neumann <neumanntylers@gmail.com>
2021-09-28 17:36:51 -0400

    Adds states geojson

---
+++ generate_geojson.R
@@ -0,0 +1,19 @@
+library(data.table)
+library(tigris)
+library(sf)
+library(sp)
+library(geojsonio)
+library(geojsonlint)
+
+options(tigris_use_cache = TRUE)
+
+all_states <- states(cb = T, resolution = "20m") |> 
+  as.data.table()
+
+all_states_df <- all_states[, .(GEOID, Name = NAME, Abbrv = STUSPS)] |> as.data.frame()
+row.names(all_states_df) <- gsub("^", "ID", row.names(all_states_df))
+
+all_states[, geometry] |> 
+  as_Spatial() |> 
+  SpatialPolygonsDataFrame(data = all_states_df) |>
+  geojson_write(file = "geojson/states")
